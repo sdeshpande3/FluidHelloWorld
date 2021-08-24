27de6c2867eaf62566a78dceee3eab9dd103720f
Sonali Deshpande <sdeshpande3@hawk.iit.edu>
2021-08-24 14:23:59 -0700

    Commit made from VS Code Azure Static Web Apps

---
+++ api/GetFrsToken/index.ts
@@ -0,0 +1,64 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { ScopeType } from "@fluidframework/protocol-definitions";
+import { generateToken, generateUser } from "@fluidframework/server-services-utils";
+
+export const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    // Parse out query parameters. tenantId and documentId are required, userId, userName, and scopes are
+    // optional and willbe filled with default values if not provided
+    const tenantId = "frs-client-tenant";
+    const documentId = "123";
+    const userId = "test-user";
+    const userName = "Test User";
+    const scopes = (req.query.scopes || (req.body && req.body.scopes)) as ScopeType[];
+
+    if (!tenantId) {
+        context.res = {
+            status: 400,
+            body: "No tenantId provided in query params",
+        };
+    }
+
+    // The private key for the given tenantId will be parsed out from the Azure Function's application settings. The value for
+    // "tenants" here is a JSON blob that holds the mapping between tenantId's and their respective secret keys
+    const key = "ac4253ece49d783e979846fb5a09b303";
+    if (!key) {
+        context.res = {
+            status: 404,
+            body: `No key found for the provided tenantId: ${tenantId}`,
+        };
+    }
+
+    if (!documentId) {
+        context.res = {
+            status: 400,
+            body: "No documentId provided in query params"
+        };
+    }
+
+    // generateUser will generate a random userName and userId. If either of these values are not provided as
+    // query parameters, the randomly generated values will be used instead
+    let user = { name: userName, id: userId };
+    if (!userId || !userName) {
+        const generatedUser = generateUser() as any;
+        user = { name: userName ?? generatedUser.name, id: userId ?? generatedUser.id };
+    }
+
+    // This will generate a token for the given user that is signed using the tenant's secret key. This allows the token
+    // to be returned to the client without ever exposing the secret itself to it. Instead, the token is generated using it
+    // to provide scoped access to the given document. This token can be returned by an ITokenProvider implementation to use
+    // with the FrsClient.
+    const token = generateToken(
+        tenantId,
+        documentId,
+        key,
+        scopes ?? [ScopeType.DocRead, ScopeType.DocWrite, ScopeType.SummaryWrite],
+        user
+    );
+
+    context.res = {
+        status: 200,
+        body: token
+    };
+};
+
+export default httpTrigger; 
