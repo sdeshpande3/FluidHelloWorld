84d61a2bb1141125230ede97b86a31a35d3f389d
Sonali Deshpande <sdeshpande3@hawk.iit.edu>
2021-08-25 14:44:18 -0700

    add tokens

--- api/GetFrsToken/index.ts
+++ api/GetFrsToken/index.ts
@@ -5,10 +5,10 @@ import { generateToken, generateUser } from "@fluidframework/server-services-uti
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     // Parse out query parameters. tenantId and documentId are required, userId, userName, and scopes are
     // optional and willbe filled with default values if not provided
-    const tenantId = (req.query.tenantId || (req.body && req.body.tenantId)) as string;
-    const documentId = (req.query.documentId || (req.body && req.body.documentId)) as string;
-    const userId = (req.query.userId || (req.body && req.body.userId)) as string;
-    const userName = (req.query.userName || (req.body && req.body.userName)) as string;
+    const tenantId = "frs-client-tenant";
+    const documentId = "123";
+    const userId = "test-user";
+    const userName = "Test User";
     const scopes = (req.query.scopes || (req.body && req.body.scopes)) as ScopeType[];
 
     if (!tenantId) {
@@ -20,7 +20,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     // The private key for the given tenantId will be parsed out from the Azure Function's application settings. The value for
     // "tenants" here is a JSON blob that holds the mapping between tenantId's and their respective secret keys
-    const key = JSON.parse(process.env["tenants"])[tenantId];
+    const key = "ac4253ece49d783e979846fb5a09b303";
     if (!key) {
         context.res = {
             status: 404,
